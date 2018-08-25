7ed8e2d559a5bbc3ae2276316ff4406617a1db24
Takeshi Kondo <takeshi.kondo@gmail.com>
2018-08-26 01:35:02 +0900

    update

--- src/main.go
+++ src/main.go
@@ -4,21 +4,21 @@ import (
 	"log"
 	"syscall/js"
 
-	u "github.com/takeshi/wasm-sandbox/src/util"
+	. "github.com/takeshi/wasm-sandbox/src/util"
 )
 
 func main() {
 	quit := make(chan struct{}, 0)
-	html, err := u.Fetch("templates/editor.html")
+	html, err := Fetch("templates/editor.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	u.S("body").InsertAdjacentHTML("beforeend", html)
+	S("body").InsertAdjacentHTML("beforeend", html)
 
-	u.S("#render").OnClick(func([]js.Value) {
-		md := u.S("#markdown").Get("value").String()
-		u.S("#preview").InnerHTML(md)
+	S("#render").OnClick(func([]js.Value) {
+		md := S("#markdown").Get("value").String()
+		S("#preview").InnerHTML(md)
 	})
 
 	<-quit
